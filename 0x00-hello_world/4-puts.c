518a65e54611a335fc2cfe877dddaed169f38cd0
Hartyplaza <ofigwehart@mail.com>
2023-02-16 13:38:51 -0800

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#includee <stdio.h>
+
+/**
+ * main - this is a main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (o);
+}
