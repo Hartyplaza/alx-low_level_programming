f80f6c730adb5cd9e50d7b5cb2c8afa13a893bd7
Hartyplaza <ofigwehart@mail.com>
2023-02-17 05:26:47 -0800

    size of int char long int and float

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - print size of data in C programming 
- * code by Hartyplaza
- * Return: 0
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("size of an int: %lu byte(S)\n", (unsigned long)sixeof(b));
-	printf("sige of a long int: %lu byte(S)", (unsigned long)sizeof(c));
-	printf("size of a long long int: %lu byte(S)", (unsigned long)sizeof(d));
-	printf("size of a float: %lu byte(S)", (unsigned long)sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
+
