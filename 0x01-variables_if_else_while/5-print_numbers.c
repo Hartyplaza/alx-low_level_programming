8fe79defe98263b86c06f7cba046833d7277d14b
Hartyplaza <ofigwehart@mail.com>
2023-02-17 16:46:36 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the main fuction
+ *
+ * Return: always 0 (successs)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
