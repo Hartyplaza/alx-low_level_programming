336337f748b3d118e8705758e79dc75ae0ac025c
Hartyplaza <ofigwehart@mail.com>
2023-02-18 04:23:34 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,12 +13,12 @@
 int main(void)
 {
 	int i;
-	char h;
+	char m;
 
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
-	for (h = 'a' ; h <= 'h' ; h++)
-		putchar(h);
+	for (m = 'a' ; m <= 'f' ; m++)
+		putchar(m);
 	putchar('\n');
 	return (0);
 }
