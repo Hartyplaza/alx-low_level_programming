e47f80e0c170de27f58063c3927605c201e424e7
Hartyplaza <you@example.com>
2023-04-05 09:01:24 -0700

    this function prints all element of linklist_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,8 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 /**
  * print_listint - prints all the elements of a linked list
  * @h: head of list
@@ -6,10 +10,9 @@
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h);
-
+size_t print_listint(const listint_t *h)
 {
-	(const listint_t *curr = h);
+	const listint_t *curr = h;
 	size_t nodes = 0;
 
 	if (!h)
