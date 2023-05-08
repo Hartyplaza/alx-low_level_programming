8e0d9993c521e2c7d49b2fc8ef5329547c68e419
hartyplaza <ofigwehart@gmail.com>
2023-05-08 16:26:11 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
-"main.h"
+#include "main.h"
 
 /**
- * create_file - this function Creates a file.
+ * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
